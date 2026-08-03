7e908aefa50b52f4d9f48302d40806efcf1faa2d
Parminder Klair <p.klair25@gmail.com>
2026-08-03 11:53:41 +0100

    chore(main): release 1.3.0

--- cli/src/assets.generated.ts
+++ cli/src/assets.generated.ts
@@ -1139,4 +1139,4 @@ SITE_URL=
 
 // cli/package.json#version (embedded so the compiled binary can self-identify
 // — used by `subwave --version`).
-export const CLI_VERSION = `1.2.0`; // x-release-please-version
+export const CLI_VERSION = `1.3.0`; // x-release-please-version
